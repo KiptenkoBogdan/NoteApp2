f4c65f0254c845fb00ae52bf1d064fcb587c7b93
KiptenkoBogdan <60790019+KiptenkoBogdan@users.noreply.github.com>
2023-05-26 17:25:39 +0300

    Added fragments for article view and editing, added "back to previous activity" button (does not work currently)

--- app/src/main/java/com/example/noteapp2/ArticleActivity.java
+++ app/src/main/java/com/example/noteapp2/ArticleActivity.java
@@ -3,6 +3,9 @@ package com.example.noteapp2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,13 +13,21 @@ import android.view.MenuItem;
 
 public class ArticleActivity extends AppCompatActivity {
 
+    private Fragment articleFragment;
+    private Fragment articleEditFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
 
-        Toolbar mainToolbar = (Toolbar) findViewById(R.id.id_article_toolbar);
-        setSupportActionBar(mainToolbar);
+        Toolbar articleToolbar = (Toolbar) findViewById(R.id.id_article_toolbar);
+        setSupportActionBar(articleToolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_backspace);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        articleEditFragment = new ArticleEditFragment();
+        articleFragment = new ArticleFragment();
+        showFragment(articleFragment);
     }
 
     @Override
@@ -28,12 +39,19 @@ public class ArticleActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if(id == R.id.id_add_tool){
-            showEditArticleFragment();
+        if(id == R.id.id_edit_tool){
+            showFragment(new ArticleEditFragment());
+        }
+        if(id == R.id.id_save_tool){
+            showFragment(new ArticleFragment());
         }
         return true;
     }
 
-    private void showEditArticleFragment() {
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.id_article_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
